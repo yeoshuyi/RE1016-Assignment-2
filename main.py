5848ae3a7f97c7767b4b2bee8302f73f87b25666
Shu <yeoshuyi123@gmail.com>
2026-02-03 14:50:36 +0800

    Finished all

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import os
 import sys
 import builtins
 import curses
+import time
+import math
 
 #I am treating assignment.py as a module through import
 #However assignment.py has a main() function without __name__ guard
@@ -44,15 +46,15 @@ class CanteenQuery:
     def search_by_keywords(self, key):
         """Return results by keyword"""
 
-        if not isinstance(key, str): return 400
-        parsed_key = self.normalize_query(key)
+        if not isinstance(key, str): return None
+        parsed_key = self.__normalize_query(key)
         # if __debug__: 
         #     print(f"[DEBUG] Query resolved as:\n{parsed_key}")
-        results = self.find_keywords(parsed_key)
+        results = self.__find_keywords(parsed_key)
         
         return results
     
-    def normalize_query(self, key):
+    def __normalize_query(self, key):
         """
         Cleans query and resolve logical conflicts (AND OR). Follows rules:
         1.  Any leading/trailing AND, OR, /s is ignored.
@@ -87,7 +89,7 @@ class CanteenQuery:
 
         return key_groups
     
-    def find_keywords(self, conditions):
+    def __find_keywords(self, conditions):
         results = []
         
         for canteen_name, stalls in self.keywords.items():
@@ -97,24 +99,54 @@ class CanteenQuery:
                 for and_group in conditions:
                     if all(req_key in stall_keywords for req_key in and_group):
                         matched_group = and_group
-                        break
+                        results.append({
+                            "canteen": canteen_name,
+                            "stall": stall_name,
+                            "keywords": stall_keywords,
+                            "matched": matched_group
+                        })
         
-            if matched_group:
-                results.append({
-                    "canteen": canteen_name,
-                    "stall": stall_name,
-                    "keywords": stall_keywords,
-                    "matched": matched_group
-                })
-
         return results
-                        
-    def search_by_location(self):
-        assignment.get_user_location_interface()
+
+    def search_by_price(self, min, max):
+        """Return results by price"""
+        
+        results = []
+        for canteen_name, stalls in self.prices.items():
+            for stall_name, price in stalls.items():
+                if min <= price <= max:
+                    results.append({
+                        "canteen": canteen_name,
+                        "stall": stall_name,
+                        "price": price
+                    })
+        return results
+
+    def search_by_location(self, k):
+        """Return k results by location"""
+
+        ax, ay = assignment.get_user_location_interface()
+        bx, by = assignment.get_user_location_interface()
+
+        results = []
+        results = []
+        for canteen_name, location in self.canteen_locations.items():
+            x, y = location
+            distance_to_A = math.sqrt((x - ax)**2 + (y - ay)**2)
+            distance_to_B = math.sqrt((x - bx)**2 + (y - by)**2)
+            max_distance = max(distance_to_A, distance_to_B)
+            results.append({
+                "canteen": canteen_name,
+                "distanceA": int(distance_to_A),
+                "distanceB": int(distance_to_B),
+                "max": int(max_distance)
+            })
+        results.sort(key=lambda x: x['max'])
+        return results[:k]
 
 
 class CurseMenu:
-    """CLI Graphical Interface"""
+    """This class handles the CLI Graphical Interface"""
 
     def __init__(self, db):
         self.db = db
@@ -142,19 +174,86 @@ class CurseMenu:
                 self.stdscr.addstr(y, x, row)
         self.stdscr.refresh()
 
-    def __get_input(self, prompt, x, y):
-        self.stdscr.addstr(x, y, prompt)
-        curses.echo()
-        user_input = self.stdscr.getstr(x+1, y, 80).decode('utf-8')
-        curses.noecho()
-        self.user_input = user_input
+    def __get_input_str(self, prompt, y, x):
+        while True:
+            self.stdscr.addstr(y, x, prompt)
+            curses.echo()
+            self.stdscr.attron(curses.color_pair(1))
+            user_input = self.stdscr.getstr(y+1, x, 80).decode('utf-8')
+            self.stdscr.attroff(curses.color_pair(1))
+            curses.noecho()
+            if not user_input: 
+                self.stdscr.addstr(y+2, x, "Input cannot be empty!")
+                continue
+            self.user_input = user_input
+            break
+    
+    def __get_input_float(self, prompt, y, x, min=0):
+            h, w = self.stdscr.getmaxyx()
+            while True:
+                self.stdscr.clear()
+                title = "--- PRICE SEARCH ---"
+                self.stdscr.attron(curses.A_BOLD)
+                self.stdscr.addstr(1, w//2 - len(title)//2, title)
+                self.stdscr.attroff(curses.A_BOLD)
+                self.stdscr.border(0)
+                self.stdscr.addstr(y, x, prompt)
+                curses.echo()
+                self.stdscr.attron(curses.color_pair(1))
+                user_input = self.stdscr.getstr(y+1, x, 80).decode('utf-8')
+                self.stdscr.attroff(curses.color_pair(1))
+                curses.noecho()
+                try:
+                    user_value = float(user_input)
+                    if user_value < min:
+                        self.stdscr.addstr(y+2, x, f"Input must be more than ${min:.2f}!")
+                        self.stdscr.refresh()
+                        time.sleep(1)
+                        continue
+                    break
+                except:
+                    self.stdscr.addstr(y+2, x, "Input must be integer/float!")
+                    self.stdscr.refresh()
+                    time.sleep(1)
+                    continue
+            return user_value
+    
+    def __get_input_int(self, prompt, y, x):
+            h, w = self.stdscr.getmaxyx()
+            while True:
+                self.stdscr.clear()
+                title = "--- LOCATION SEARCH ---"
+                self.stdscr.attron(curses.A_BOLD)
+                self.stdscr.addstr(1, w//2 - len(title)//2, title)
+                self.stdscr.attroff(curses.A_BOLD)
+                self.stdscr.border(0)
+                self.stdscr.addstr(y, x, prompt)
+                curses.echo()
+                self.stdscr.attron(curses.color_pair(1))
+                user_input = self.stdscr.getstr(y+1, x, 80).decode('utf-8')
+                self.stdscr.attroff(curses.color_pair(1))
+                curses.noecho()
+                try:
+                    user_value = int(user_input)
+                    if user_value < 1:
+                        self.stdscr.addstr(y+2, x, f"Input must be more than 0!")
+                        self.stdscr.refresh()
+                        time.sleep(1)
+                        continue
+                    break
+                except:
+                    self.stdscr.addstr(y+2, x, "Input must be integer!")
+                    self.stdscr.refresh()
+                    time.sleep(1)
+                    continue
+            return user_value
 
     def main_menu(self, stdscr):
         self.stdscr = stdscr
         h, w = self.stdscr.getmaxyx()
-        if w < 150 or h < 20:
+        if w < 100 or h < 20:
             raise SystemError(
-                f"[ERROR] Please expand your CLI to at least 20x150. "
+                f"[ERROR] Please expand your CLI to at least 20x100. "
                 f"This is to prevent grahical glitches."
             )
         
@@ -206,8 +305,7 @@ class CurseMenu:
                         self.stdscr.addstr(h-2, start_x, "Press any key to return...")
                         self.stdscr.refresh()
                         self.stdscr.getch()
-                        
-                    
+                                           
                     case 1:
                         self.stdscr.clear()
                         title = "--- KEYWORD SEARCH ---"
@@ -215,10 +313,7 @@ class CurseMenu:
                         self.stdscr.addstr(1, w//2 - len(title)//2, title)
                         self.stdscr.attroff(curses.A_BOLD)
                         self.stdscr.border(0)
-                        
-                        col_canteen = 25
-                        col_stall = 25
-                        self.__get_input("Enter Search Query:", 4, 2)
+                        self.__get_input_str("Enter Search Query:", 4, 2)
                         results = self.db.search_by_keywords(self.user_input)
 
                         self.stdscr.clear()
@@ -231,8 +326,7 @@ class CurseMenu:
                         if not results:
                             self.stdscr.addstr(h//2, w//2 - 10, "No matches found.")
                         else:
-
-                            header_str = f"{'CANTEEN'.ljust(col_canteen)} {'STALL'.ljust(col_stall)} {'  MATCHED'}"
+                            header_str = f"{'CANTEEN'.ljust(25)} {'STALL'.ljust(27)} {'MATCHED'}"
                             self.stdscr.addstr(3, 4, header_str.ljust(w-8)) 
                             
                             for idx, match in enumerate(results):
@@ -241,8 +335,8 @@ class CurseMenu:
                                     self.stdscr.addstr(y, 4, "... more results hidden ...", curses.A_DIM)
                                     break
                                 
-                                c_name = match['canteen'][:col_canteen-1].ljust(col_canteen)
-                                s_name = match['stall'][:col_stall-1].ljust(col_stall)
+                                c_name = match['canteen'][:24].ljust(25)
+                                s_name = match['stall'][:24].ljust(25)
                                 criteria = str(match['matched'])
 
                                 row_str = f"{c_name} | {s_name} | {criteria}"
@@ -250,29 +344,95 @@ class CurseMenu:
                         self.stdscr.getch()
                     
                     case 2:
+                        min = self.__get_input_float("Enter Minimum Price: ", 4, 2, 0.00)
+                        max = self.__get_input_float("Enter Maximum Price: ", 4, 2, min)
+                        results = self.db.search_by_price(min, max)
+
                         self.stdscr.clear()
-                        title = "--- PRICE SEARCH ---"
+                        title = "--- SEARCH RESULTS ---"
                         self.stdscr.attron(curses.A_BOLD)
                         self.stdscr.addstr(1, w//2 - len(title)//2, title)
                         self.stdscr.attroff(curses.A_BOLD)
                         self.stdscr.border(0)
 
+                        if not results:
+                            self.stdscr.addstr(h//2, w//2 - 10, "No matches found.")
+                        else:
+                            header_str = f"{'CANTEEN'.ljust(25)} {'STALL'.ljust(27)} {'PRICE'}"
+                            self.stdscr.addstr(3, 4, header_str.ljust(w-8)) 
+                            
+                            for idx, match in enumerate(results):
+                                y = 4 + idx
+                                if y >= h - 3:
+                                    self.stdscr.addstr(y, 4, "... more results hidden ...", curses.A_DIM)
+                                    break
+                                
+                                c_name = match['canteen'][:24].ljust(25)
+                                s_name = match['stall'][:24].ljust(25)
+                                criteria = str(match['price'])
+
+                                row_str = f"{c_name} | {s_name} | {criteria}"
+                                self.stdscr.addstr(y, 4, row_str)
                         self.stdscr.getch()
 
                     case 3:
                         self.stdscr.clear()
+                        stall_count = self.__get_input_int("Enter Number of Stalls: ", 4, 2)
 
-                        self.db.search_by_location()
+                        self.stdscr.clear()
                         title = "--- LOCATION SEARCH ---"
                         self.stdscr.attron(curses.A_BOLD)
                         self.stdscr.addstr(1, w//2 - len(title)//2, title)
                         self.stdscr.attroff(curses.A_BOLD)
                         self.stdscr.border(0)
 
+                        self.stdscr.addstr(4, 2, "Please select 2 locations on the map.")
+                        self.stdscr.refresh()
+                        time.sleep(1)
+
+                        self.stdscr.clear()
+
+                        results = self.db.search_by_location(stall_count)
+                        title = "--- LOCATION SEARCH ---"
+                        self.stdscr.attron(curses.A_BOLD)
+                        self.stdscr.addstr(1, w//2 - len(title)//2, title)
+                        self.stdscr.attroff(curses.A_BOLD)
+                        self.stdscr.border(0)
+
+                        if not results:
+                            self.stdscr.addstr(h//2, w//2 - 10, "No matches found.")
+                        else:
+                            header_str = f"{'CANTEEN'.ljust(25)} {'DISTANCE TO USER A / m'.ljust(25)} {'DISTANCE TO USER B / m'.ljust(25)} {'MAX DISTANCE / m'.ljust(25)}"
+                            self.stdscr.addstr(3, 4, header_str.ljust(w-8)) 
+                            
+                            for idx, match in enumerate(results):
+                                y = 4 + idx
+                                if y >= h - 3:
+                                    self.stdscr.addstr(y, 4, "... more results hidden ...", curses.A_DIM)
+                                    break
+                                
+                                c_name = match['canteen'][:24].ljust(25)
+                                disA = str(match['distanceA']).ljust(23)
+                                disB = str(match['distanceB']).ljust(23)
+                                max_dis = str(match['max'])
+
+                                row_str = f"{c_name} | {disA} | {disB} | {max_dis}"
+                                self.stdscr.addstr(y, 4, row_str)
+
                         self.stdscr.getch()
                     
                     case 4:
+                        self.stdscr.clear()
+                        title = "--- THANK YOU! GOODBYE! ---"
+                        self.stdscr.attron(curses.A_BOLD)
+                        self.stdscr.addstr(1, w//2 - len(title)//2, title)
+                        self.stdscr.attroff(curses.A_BOLD)
+                        self.stdscr.border(0)
+                        self.stdscr.refresh()
+                        time.sleep(1)
+                        self.stdscr.clear()
                         break
+
 
 def main():
     """Main function for user interface"""
@@ -280,8 +440,7 @@ def main():
     #Db and Menu Init
     db = CanteenQuery(path=DATABASE_PATH)
     menu = CurseMenu(db)
-    curses.wrapper(menu.main_menu)
-            
+    curses.wrapper(menu.main_menu)            
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,16 +1,16 @@
-## CAA: 13-Jan-2025
+## CAA: 03-Feb-2026
 
 ### Acknowledgements
 Written by Yeo Shu Yi, for RE1016 Individual Assignment.</br>
-Much of this program is refactored from the original assignment.py. </br>
 
 * **Dr. Ong Yew Soon** - Author of original assignment.py.
 
 ### File Management
-- /src directory contains resources (E.g. images, excel file).
+**No modifications done to assignment.py! main.py calls assignment.py as module**
 - /testbench directory contains test files for specific code sections.
 - /old directory contains the original refactored assignment.py.
 - main.py is the main program.
+- assignment.py is the given module.
 - changelog.md contains commit history.
 - requirements.txt contains PIP library requirements.
 
@@ -19,23 +19,23 @@ Much of this program is refactored from the original assignment.py. </br>
    ```bash
    cd <REPO PATH>
    git clone https://github.com/yeoshuyi/RE1016-Assignment-2.git
+   cd RE1016-Assignment-2
 2. Install PIP libraries with venv (optional):</br>
    ```bash
-   #OPTIONAL VENV SETUP
+   #OPTIONAL VENV SETUP (May differ based on OS)
    python3 -m venv .venv
    source ./.venv/bin/activate
 
    #INSTALL LIBRARY
    python3 -m pip install -r requirements.txt
 3. Run main.py:</br>
+**Ensure CLI window is at least 20x100, or SystemError will be raised to avoid graphical glitches.**
    ```bash
    python3 -O main.py #Run without -O flag to see debug prints
-
 ### Changelog
 Refer to changelog.md.
 
 ### External Libraries
-External libraries as per assignment requirements.</br>
-Addition of: (TBC)</br>
+External libraries as per assignment requirements with no additions made.</br>
 Refer to requirements.txt for full list.</br>
 
